6e48df5bbd541347719012209a5a453d27bdd46e
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2019-05-29 01:01:10 +0300

    Added Home top page
    D - Desktop
    M- Mobile
    - added the moving balls (D&M)
    - added the main menu (D&M)
    -added translations
    - added services module (D&M)
    - added loading screen (D&M)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,29 @@ function test() {
     console.log(123)
 }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
 jQuery.fn.scrollCenter = function (elem, speed) {
     var active = jQuery(this).find(elem);
     var activeWidth = active.width() / 2;
@@ -23,26 +46,86 @@ jQuery.fn.scrollCenterORI = function (elem, speed) {
     return this;
 };
 
+function showHideButton () {
+    jQuery(".pre_loader button.next").show();
+    setCookie("loader","true","2")
+}
+
 function bodyAnimation() {
     $("body").addClass("animating");
     $("body").addClass("slide-in-right");
     if ($("body").hasClass("dark_theme_bg")) {
-        $("body").addClass("light_theme_bg");
-        $("body").removeClass("dark_theme_bg");
+        $("nav").addClass("light_theme_bg");
+        $("nav").removeClass("dark_theme_bg");
+        setTimeout(function (event) {
+            $("body").addClass("light_theme_bg");
+            $("body").removeClass("dark_theme_bg");
+        }, 2000); 
         $(".switch_img").attr("src","assets/img/toggle/dark_theme.svg");
     } else {
-        $("body").addClass("dark_theme_bg");
-        $("body").removeClass("light_theme_bg");
+        setTimeout(function (event) {
+            $("body").addClass("dark_theme_bg");
+            $("body").removeClass("light_theme_bg");
+        }, 2000); 
+        $("nav").removeClass("light_theme_bg");
+        $("nav").addClass("dark_theme_bg");
         $(".switch_img").attr("src","assets/img/toggle/white_theme.svg")
     }
     setTimeout(function (event) {
         $("body").removeClass("animating");
         $("body").removeClass("slide-in-right");
-    }, 1000);
-    
+    }, 1000);   
+}
+
+function removeLoader() {
+    jQuery(".pre_loader").remove();
 }
 
 $(document).ready(function () {
+    var loaderStatus = getCookie("loader");
+    if (loaderStatus == "true") {
+        removeLoader();
+    } else {
+        setTimeout(function (event) {
+            showHideButton();
+        },2000);
+    }
+    
+
+    jQuery(document).on("click", '.pre_loader button.next', function (event) {
+        removeLoader();
+    });
+    test();
+    jQuery(document).on("click", 'img.menu_toggle', function (event) {
+        // test();
+        if ($("nav.top_main_menu").hasClass("animate")) {
+            jQuery("nav.top_main_menu").removeClass("animate");
+        } else {
+            jQuery("nav.top_main_menu").addClass("animate");
+        }
+    });
+
+    var oldLang = "ro";
+
+    jQuery(document).on("click", '.languages span.lang', function (event) {
+        var newLang = jQuery(this).attr("data-lang");
+        // console.log(oldLang)
+        if (oldLang != newLang) {
+            $('*[data-'+newLang+']').each(function() {
+                var newText = jQuery(this).attr("data-"+newLang);
+                var oldText = jQuery(this).text();
+                $(this).attr("data-"+oldLang, oldText);
+                $(this).text(newText);
+                console.log(newText)
+            });
+            oldLang = newLang;
+            // console.log(oldLang)
+            jQuery("nav.top_main_menu").removeClass("animate");
+        }
+        
+        
+    });
+
     jQuery(document).on("click", 'h1', function (event) {
         try {
             $(this).addClass("verified");
